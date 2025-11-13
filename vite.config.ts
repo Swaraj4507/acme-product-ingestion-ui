efec06ab6c84533d5cdb86f15ea5316060e1d3aa
Swaraj Sampathirao <swarajsampathirao8330@gmail.com>
2025-11-13 23:30:12 +0530

    Initialize React project with Vite, ESLint, and Tailwind CSS setup.

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { fileURLToPath } from 'node:url'
+import { dirname, resolve } from 'node:path'
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+})
+
